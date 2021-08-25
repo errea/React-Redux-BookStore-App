f17fa4b6b6714d9f8eecb96bc4d1cccee877bc41
errea <erreakezy@gmail.com>
2021-08-25 12:20:17 +0100

    Review-fix:modify App.js file

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -6,56 +6,25 @@ import {
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
-import Nav from './components/navBar';
-import Book from './components/books';
-import BookNew from './components/bookNew';
-import Categories from './components/categories';
+import Nav from './components/NavBar';
+import BookStore from './components/BookStore';
+import BookNew from './components/BookNew';
+import Categories from './components/Categories';
 
-const booksInfo = [
-  {
-    id: 1,
-    category: 'Action',
-    name: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    percentage: '64%',
-    chapter: 'Chapter 17',
-  },
-  {
-    id: 2,
-    category: 'Science Fiction',
-    name: 'Dune',
-    author: 'Frank Herbert',
-    percentage: '8%',
-    chapter: 'Chapter 3: "A Lesson Learned"',
-  },
-  {
-    id: 3,
-    category: 'Economy',
-    name: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    percentage: '0%',
-    chapter: 'Introduction',
-  },
-];
-
-const App = () => {
-  const bookList = booksInfo.map((bookInfo) => <Book key={bookInfo.id} info={bookInfo} />);
-  return (
-    <Provider store={store}>
-      <Router>
-        <Nav />
-        <Switch>
-          <Route exact path="/">
-            { bookList }
-            <BookNew />
-          </Route>
-          <Route path="/categories">
-            <Categories />
-          </Route>
-        </Switch>
-      </Router>
-    </Provider>
-  );
-};
-
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <BookStore />
+          <BookNew />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </Router>
+  </Provider>
+);
 export default App;
