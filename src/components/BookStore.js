48384b2efbbf2c3b89f4779f1b51529acb9b241e
errea <erreakezy@gmail.com>
2021-08-26 15:20:29 +0100

    Add getBooks method from bookSlice function

--- src/components/BookStore.js
+++ src/components/BookStore.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Book from './books';
+import getBooks from '../redux/slices/bookSlice';
 
 const BookStore = () => {
-  const bookStore = useSelector((store) => store.booksReducer);
-  const bookComponent = bookStore.map(
-    (book) => (
-      <Book
-        key={book.id}
-        id={book.id}
-        title={book.title}
-        author={book.author}
-        category={book.category}
-      />
-    ),
-  );
+  const bookComponent = [];
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
+
+  let bookStoreFetch = useSelector((store) => store.booksReducer.books);
+  if (bookStoreFetch.error) {
+    bookStoreFetch = [];
+  } else {
+    const bookIDs = Object.keys(bookStoreFetch);
+    bookIDs.forEach((id) => {
+      bookStoreFetch[id].map((book) => (
+        bookComponent.push(
+          <Book
+            key={id}
+            id={id}
+            title={book.title}
+            category={book.category}
+          />,
+        )
+      ));
+    });
+  }
 
   return (
     <>
